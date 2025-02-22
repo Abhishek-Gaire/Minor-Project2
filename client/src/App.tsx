8e6ad0505b543b6ca6ae1e0d1dc62a2141ddfb1e
Abhishek Gaire <abhisekgaire7@gmail.com>
2025-02-22 22:39:13 +0545

    fix: created routes file for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,6 @@
-// import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import { HomePage } from "./pages/HomePage.tsx";
-import { AboutPage } from "./pages/AboutPage.tsx";
-import { ContactPage } from "./pages/ContactPage.tsx";
-import { FeaturesPage } from "./pages/FeaturesPage.tsx";
-import { InstitutionSignup } from "./pages/InstitutionSignup";
-import { Login } from "./pages/Login.tsx";
-import { RouteNotFound } from "./pages/RouteNotFound";
-import { Signup } from "./pages/Signup.tsx";
-import { MeetingRoom } from "./components/Meetings/MeetingRoom.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes"; // Import the routes
 import { AuthProvider } from "./contexts/AuthContext.tsx";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // Import Toastify styles
 
@@ -31,20 +20,7 @@ export default function App() {
         pauseOnHover
         theme="light"
       />
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/features" element={<FeaturesPage />} />
-          <Route path="/institution-signup" element={<InstitutionSignup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/meeting/:meetingId" element={<MeetingRoom />} />
-          {/*Other Routes*/}
-          <Route path="*" element={<RouteNotFound />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 }
