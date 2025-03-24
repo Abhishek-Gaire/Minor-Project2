e28397549c1c8fd8f011b354ea8233896caca668
Abhishek Gaire <abhisekgaire7@gmail.com>
2025-03-24 15:54:01 +0545

    fix: ThemeConetxt Added

---
+++ client/src/contexts/ThemeContext.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useEffect, useState } from "react";
+
+type Theme = "dark" | "light" | "system";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+}
+
+interface ThemeContextType {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+}
+
+const initialState: ThemeContextType = {
+  theme: "system",
+  setTheme: () => null,
+};
+
+const ThemeContext = createContext<ThemeContextType>(initialState);
+
+export function ThemeProvider({
+  children,
+  defaultTheme = "system",
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove("light", "dark");
+
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme:dark)")
+        .matches
+        ? "dark"
+        : "light";
+      root.classList.add(systemTheme);
+      return;
+    }
+
+    root.classList.add(theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export default ThemeContext;
