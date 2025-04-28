736c3c4aa83ec2c202d78baf29609ae4d303418c
Abhishek Gaire <abhisekgaire7@gmail.com>
2025-04-28 14:22:40 +0545

    feat: adeed frontend url in cors

--- server/src/config/corsConfig.ts
+++ server/src/config/corsConfig.ts
@@ -1,12 +1,16 @@
 import cors from "cors";
 import getLocalIP from "../exceptions/getLocalIP";
+import dotenv from "dotenv";
 
 const ip = getLocalIP();
+dotenv.config();
 
+const FRONTEND_URL = process.env.FRONTEND_URL;
 const corsOptions = {
   origin: [
     "http://localhost:5173",
     `http://${ip}:5173`,
+    `${FRONTEND_URL}`
     // Add any other network URLs you need
   ],
   credentials: true,
